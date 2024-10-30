3700918fc27152c6c0bdbddbb869a2569702cad7
pavan147 <pavanmgawande@gmail.com>
2024-10-30 21:15:14 +0530

    per-Post filter

---
+++ src/main/java/com/example/apigateway/PreFilter.java
@@ -0,0 +1,24 @@
+package com.example.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class PreFilter implements GlobalFilter {
+    final Logger logger = LoggerFactory.getLogger(PreFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Pre Filter executed :");
+
+        String requestPath = exchange.getRequest().getPath().toString();
+        logger.info("Pre Filter Request path = " + requestPath);
+
+        return chain.filter(exchange);
+    }
+}
